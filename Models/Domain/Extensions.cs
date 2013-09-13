1c73d08ddc7804bd948a1f3239a4d8567cfad445
Josh <josh@magicmoments.com>
2013-09-13 18:45:09 -0400

    added domain extensions for refs

---
+++ Models/Domain/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JoshCodes.Web.Models.Domain
+{
+    public static class Extensions
+    {
+        public static DomainId GetDomainId(this Persistence.IDefineModelObject modelObject)
+        {
+            return new DomainId(modelObject.IdKey, modelObject.IdGuid, modelObject.IdUrn);
+        }
+    }
+}
