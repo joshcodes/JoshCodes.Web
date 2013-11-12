343427ecb20a15ad5ea5c9a41f20981eb093e24a
Josh <josh@magicmoments.com>
2013-11-12 13:27:38 -0500

    added KVP store implementation

---
+++ Models/Persistence/IDefineKvp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoshCodes.Web.Models.Persistence
+{
+    public interface IDefineKvp : IDefineModelObject
+    {
+        string Container { get; }
+        string LookupKey { get; }
+        string Value { get; }
+    }
+}
---
+++ Models/Persistence/IStoreKvp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace JoshCodes.Web.Models.Persistence
+{
+    public interface IStoreKvp : IStoreObjects<IDefineKvp>
+    {
+        string Get(string container, string key);
+
+        void Create(string container, string key, string value);
+    }
+}
