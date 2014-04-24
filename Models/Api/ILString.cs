35bc43c207d8b06cc45d2e12a365bee7b9182d04
joshcodes <josh.wingstrom@lonelyplanet.com>
2014-04-24 09:17:36 -0500

    added international string

---
+++ Models/Api/ILString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JoshCodes.Web.Models.Api
+{
+    public class ILString
+    {
+        public ILString(string en)
+        {
+            this.En = en;
+        }
+        public string En { get; set; }
+
+        public string Es { get; set; }
+
+        public string Fr { get; set; }
+    }
+}
