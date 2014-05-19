efb7550bcc7ec950615fd2332f233e005b03667e
joshcodes <josh.wingstrom@lonelyplanet.com>
2014-05-19 11:57:10 -0500

    added serialization constructs to ILString

--- Models/Api/ILString.cs
+++ Models/Api/ILString.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace JoshCodes.Web.Models.Api
 {
+    // TODO: Convert to T4
+    [DataContract()]
     public class ILString
     {
         public ILString(string en)
         {
             this.En = en;
         }
+
+        [DataMember()]
         public string En { get; set; }
 
+        [DataMember()]
         public string Es { get; set; }
 
+        [DataMember()]
         public string Fr { get; set; }
     }
 }
