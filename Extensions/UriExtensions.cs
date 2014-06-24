7f763ac8915f07671031f9661b1a3fa78a94a369
joshcodes <josh.wingstrom@lonelyplanet.com>
2014-06-24 18:33:56 -0500

    added ToStream() to uri extensions

---
+++ Extensions/UriExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JoshCodes.Web
+{
+    public static class UriExtensions
+    {
+        public static Stream ToStream(this Uri url)
+        {
+            byte[] imageData = null;
+
+            using (var wc = new System.Net.WebClient())
+                imageData = wc.DownloadData(url);
+
+            return new MemoryStream(imageData);
+        }
+    }
+}
