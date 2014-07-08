7a8fbd759d0b4fe500c5e0fdf5536f137c4910aa
joshcodes <josh.wingstrom@lonelyplanet.com>
2014-07-08 07:43:10 -0500

    added metadata

---
+++ Metadata/Name.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NC2.CPM.Metadata
+{
+    class Name
+    {
+    }
+}
---
+++ Metadata/Phone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NC2.CPM.Metadata
+{
+    public class Phone
+    {
+        string phone;
+
+
+
+        public String AreaCode
+        {
+            get
+            {
+                // TODO: Look for -'s, etc -- generally be more smart about this
+                return phone.Substring(0, 3);
+            }
+        }
+    }
+}
